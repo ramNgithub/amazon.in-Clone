6f02209e06a7688861b2815414c1624844563487
abhayfaldu <abhayfaldu1922@gmail.com>
2023-01-19 22:46:36 +0530

    created product list

--- amazon-in/src/Components/Homepage/MainRoutes/MainRoutes.jsx
+++ amazon-in/src/Components/Homepage/MainRoutes/MainRoutes.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { useState } from 'react';
-import {Routes,Route} from "react-router-dom";
-import { auth } from '../firebase';
-import Homepage from '../Home/Homepage';
-import Login from '../Login/Login';
-import Signup from '../Signup/Signup';
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import MobilePage from "../../MobilePage/Components/MobilePage";
+import { auth } from "../firebase";
+import Homepage from "../Home/Homepage";
+import Login from "../Login/Login";
+import Signup from "../Signup/Signup";
 
 const MainRoutes = () => {
-    const [userName,setuserName]=useState("");
-    useEffect(()=>{
-      auth.onAuthStateChanged((user)=>{
-        if(user){
-            setuserName(user.displayName)
-        }else{
-            setuserName("");
-        }
-     })
-    },[])
-   
-  return (
-    <div>
-        <Routes>
-            <Route path='/' element={<Homepage name={userName}/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/signup' element={<Signup/>}/>
-        </Routes>
-       
-    </div>
-  )
-}
+	const [userName, setuserName] = useState("");
+	useEffect(() => {
+		auth.onAuthStateChanged(user => {
+			if (user) {
+				setuserName(user.displayName);
+			} else {
+				setuserName("");
+			}
+		});
+	}, []);
 
-export default MainRoutes
+	return (
+		<div>
+			<Routes>
+				<Route path="/" element={<Homepage name={userName} />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/mobile" element={<MobilePage />} />
+			</Routes>
+		</div>
+	);
+};
+
+export default MainRoutes;
---
+++ amazon-in/src/Components/MobilePage/Utils/api.js
@@ -0,0 +1,10 @@
+// import axios from "axios";
+
+// export const getData = link => {
+// 	axios(link)
+// 		.then(res => {
+// 			console.log("in api:", res.data);
+// 			res.data;
+// 		})
+// 		.catch(err => console.log("error in getting data:", err));
+// };
