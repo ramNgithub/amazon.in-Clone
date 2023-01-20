7be96c180c03f8dda3de3380db5cc2e8c6dc00c3
abhayfaldu <abhayfaldu1922@gmail.com>
2023-01-20 20:37:02 +0530

    created single product page

--- amazon-in/src/Components/MobilePage/Components/MobilePage.jsx
+++ amazon-in/src/Components/MobilePage/Components/MobilePage.jsx
@@ -1,52 +1,30 @@
-import {
-	Center,
-	Divider,
-	Grid,
-	GridItem,
-	SimpleGrid,
-	Tab,
-	TabList,
-	TabPanel,
-	TabPanels,
-	Tabs,
-	Text,
-} from "@chakra-ui/react";
+import { ChakraProvider, Grid, GridItem, SimpleGrid } from "@chakra-ui/react";
 import React from "react";
-import ProductsList from "./ProducstList";
+import Navbar from "../../Homepage/Navbar/Navbar";
+import ProductsSection from "./ProductsSection";
 import Sidebar from "./Sidebar";
 
 const MobilePage = () => {
 	return (
-		<SimpleGrid>
-			{/* navbar goes here */}
-			<Center height={"4rem"}>Nav</Center>
+		<ChakraProvider>
+			<SimpleGrid>
+				{/* navbar goes here */}
+				<Navbar />
 
-			{/* main content */}
-			<Grid
-				gridTemplateColumns={["1fr", "1fr", "1fr", "1fr", "2fr 9fr"]}
-				h="90vh"
-			>
-				<GridItem pt={4} pl={4} >
-					<Sidebar></Sidebar>
-				</GridItem>
-				<GridItem p={4}>
-					<Tabs variant="enclosed">
-						<TabList>
-							<Tab>Accessories</Tab>
-							<Tab>SmartPhones</Tab>
-						</TabList>
-						<TabPanels>
-							<TabPanel p={0} py={4}>
-								<ProductsList link="http://localhost:8080/mobileAccessories" />
-							</TabPanel>
-							<TabPanel>
-								<ProductsList link="http://localhost:8080/smartphones" />
-							</TabPanel>
-						</TabPanels>
-					</Tabs>
-				</GridItem>
-			</Grid>
-		</SimpleGrid>
+				{/* main content */}
+				<Grid
+					gridTemplateColumns={["1fr", "1fr", "1fr", "1fr", "2fr 9fr"]}
+					h="90vh"
+				>
+					<GridItem pt={4} pl={4}>
+						<Sidebar></Sidebar>
+					</GridItem>
+					<GridItem p={4}>
+						<ProductsSection></ProductsSection>
+					</GridItem>
+				</Grid>
+			</SimpleGrid>
+		</ChakraProvider>
 	);
 };
 
