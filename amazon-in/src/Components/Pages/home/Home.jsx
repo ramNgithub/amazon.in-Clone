b5d05d0f348b296aa795c7bf2d9b19e5fb7ac50e
Ram Naphade <105856357+ramNgithub@users.noreply.github.com>
2023-01-22 18:30:11 +0530

    Update Home.jsx

--- amazon-in/src/Components/Pages/home/Home.jsx
+++ amazon-in/src/Components/Pages/home/Home.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import Sidebar from "../../componentsAdmin/sidebar/Sidebar";
 import Navbar from "../../componentsAdmin/navbar/Navbar";
 import Widget from "../../componentsAdmin/widget/Widget";
+import Featured from "../../componentsAdmin/featured/Featured";
+import Chart from "../../componentsAdmin/chart/Chart";
+import Table from "../../componentsAdmin/table/Table";
 import "./home.css";
+// import "../..style/dark.css"
 
 const Home = () => {
   return (
-    <div className="home">
+    <div className="home dark">
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
@@ -17,6 +21,12 @@ const Home = () => {
           <Widget type="balance" />
         </div>
         <div className="charts">
+          <Featured/>
+          <Chart/>
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table/>
         </div>
       </div>
     </div>
